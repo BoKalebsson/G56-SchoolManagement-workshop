2438b562c6c4c822b5084504d5975dade39f51c0
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-31 16:35:40 +0200

    Added save()-method.

--- src/main/java/se/lexicon/dao/impl/CourseDaoImpl.java
+++ src/main/java/se/lexicon/dao/impl/CourseDaoImpl.java
@@ -1,4 +1,58 @@
 package se.lexicon.dao.impl;
 
-public class CourseDaoImpl {
+import se.lexicon.dao.CourseDao;
+import se.lexicon.model.Course;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseDaoImpl implements CourseDao {
+
+    private List<Course> courses = new ArrayList<>();
+
+    @Override
+    public Course save(Course course) {
+
+        // Check if course is null:
+        if(course == null){
+            throw new IllegalArgumentException("Course is not allowed to be null.");
+        }
+
+        // Check if course already exists:
+        if (courses.contains(course)) {
+            throw new IllegalArgumentException("Course already exists: " + course);
+        }
+
+        // Add the course to the list:
+        courses.add(course);
+
+        // Return the course:
+        return course;
+    }
+
+    @Override
+    public Course findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Course> findByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Course> findByDate(LocalDate date) {
+        return List.of();
+    }
+
+    @Override
+    public List<Course> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Boolean delete(Course course) {
+        return null;
+    }
 }
